04977d10d46ecdc240acbe631acef21e46e4f14c
SoupyKestrel <105160530+ANCHIT12345@users.noreply.github.com>
2025-09-27 23:20:26 +0530

    11th commit

---
+++ C-Sharp/ConsoleApp 5/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace dbmsApp1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //DisplayConnectionLibrary();
+            //DisplayBooksLibrary();
+            //DisplayStudent();
+            //DisplayStudentCount();
+            //DisplayTotalBooksCount();
+            InsertNewStudent();
+            Console.ReadKey();
+        }
+        public static void DisplayConnectionLibrary()
+        {
+            string connString = "Data Source=LAPTOP-TH0TP9P1\\SQLEXPRESS;Initial Catalog=Stored_Procedure_Assignment;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("Database Connection Status-" + conn.State);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+        public static void DisplayBooksLibrary()
+        {
+            string connString = "Data Source=LAPTOP-TH0TP9P1\\SQLEXPRESS;Initial Catalog=Stored_Procedure_Assignment;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand sqlCmd = new SqlCommand("SELECT * FROM Books;", conn);
+                    SqlDataReader reader = sqlCmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"BookId -{reader["BookId"]},Title -{reader["Title"]},Author -{reader["Author"]},Genre -{reader["Genre"]},Price -{reader["Price"]},StockQty -{reader["StockQty"]}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+        public static void DisplayStudent()
+        {
+            string connString = "Data Source=LAPTOP-TH0TP9P1\\SQLEXPRESS;Initial Catalog=Stored_Procedure_Assignment;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand sqlCmd = new SqlCommand("SELECT * FROM Students;", conn);
+                    SqlDataReader reader = sqlCmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Student ID -{reader["StudentID"]},Student Name -{reader["StudentName"]},Age -{reader["Age"]},Course -{reader["Course"]}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+        public static void DisplayStudentCount()
+        {
+            string connString = "Data Source=LAPTOP-TH0TP9P1\\SQLEXPRESS;Initial Catalog=Stored_Procedure_Assignment;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand sqlCmd = new SqlCommand("SELECT Count(*) FROM Students;", conn);
+                    int studentCount = (int)sqlCmd.ExecuteScalar();
+                    Console.WriteLine($"Student Count : {studentCount}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+        public static void DisplayTotalBooksCount()
+        {
+            string connString = "Data Source=LAPTOP-TH0TP9P1\\SQLEXPRESS;Initial Catalog=Stored_Procedure_Assignment;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand sqlCmd = new SqlCommand("SELECT Count(*) FROM Books;", conn);
+                    int BooksCount = (int)sqlCmd.ExecuteScalar();
+                    Console.WriteLine($"Books Count : {BooksCount}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+        public static void InsertNewStudent()
+        {
+            string connString = "Data Source=LAPTOP-TH0TP9P1\\SQLEXPRESS;Initial Catalog=Stored_Procedure_Assignment;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand sqlCmd = new SqlCommand("SELECT Count(*) FROM Books;", conn);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ C-Sharp/ConsoleApp 5/demo.cs
@@ -0,0 +1,109 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DatabaseProject
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //DisplayEmployee();
+//            //InsertEmployee();
+
+//            GetMaxEmployeeSalary();
+
+//            Console.Read();
+
+//        }
+//        public static void DisplayEmployee()
+//        {
+//            string connString = "Data Source=ISSLT1220;Initial Catalog=Demo;User id=sa;Password=inube@123";
+
+//            using (SqlConnection conn = new SqlConnection(connString))
+//            {
+//                try
+//                {
+//                    conn.Open();
+
+
+//                    SqlCommand sqlCmd = new SqlCommand("select EmployeeName,Design,salary from Employee", conn);
+//                    SqlDataReader reader = sqlCmd.ExecuteReader();
+
+//                    while (reader.Read())
+//                    {
+//                        Console.WriteLine($"Name -{reader["EmployeeName"]},Designation -{reader["Design"]}");
+//                    }
+//                    //Console.WriteLine("Database Connection Status-"+conn.State);
+//                    //Console.WriteLine("Database Connection Successful!");
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine("Connection Failed: " + ex.Message);
+//                }
+//            }
+
+
+//        }
+//        public static void GetMaxEmployeeSalary()
+//        {
+//            string connString = "Data Source=ISSLT1220;Initial Catalog=Demo;User id=sa;Password=inube@123";
+
+//            using (SqlConnection conn = new SqlConnection(connString))
+//            {
+//                try
+//                {
+//                    conn.Open();
+
+
+//                    SqlCommand sqlCmd = new SqlCommand("select Max(salary) from [dbo].[Employee]", conn);
+//                    //float MaxSalary = (float)sqlCmd.ExecuteScalar();
+
+//                    decimal maxValue = (decimal)sqlCmd.ExecuteScalar();
+
+
+
+//                    Console.WriteLine($"Employee Count -{maxValue}");
+//                    //Console.WriteLine("Database Connection Status-"+conn.State);
+//                    //Console.WriteLine("Database Connection Successful!");
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine("Connection Failed: " + ex.Message);
+//                }
+//            }
+
+
+//        }
+//        public static void InsertEmployee()
+//        {
+//            string connString = "Data Source=ISSLT1220;Initial Catalog=Demo;User id=sa;Password=inube@123";
+
+//            using (SqlConnection conn = new SqlConnection(connString))
+//            {
+//                try
+//                {
+//                    conn.Open();
+
+
+//                    SqlCommand sqlCmd = new SqlCommand("INSERT INTO Employee(EmployeeName,Design,salary,ManagerId,DepartmentId) " +
+//                        "values('Amarpreet', 'Web Develoer', 110000.00, 4, 1)", conn);
+
+//                    int rowAffected = sqlCmd.ExecuteNonQuery();
+
+//                    //Console.WriteLine("Database Connection Status-"+conn.State);
+//                    Console.WriteLine("Employee Inserted Successfully!");
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine("Connection Failed: " + ex.Message);
+//                }
+//            }
+
+
+//        }
+//    }
+//}
