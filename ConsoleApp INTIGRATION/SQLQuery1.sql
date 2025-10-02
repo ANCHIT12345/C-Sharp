393386771696641774ebd15dc66fbd4e527ef1c1
SoupyKestrel <105160530+ANCHIT12345@users.noreply.github.com>
2025-10-02 16:26:31 +0530

    fixes

---
+++ ConsoleApp INTIGRATION/SQLQuery1.sql
@@ -0,0 +1,53 @@
+--CREATE TABLE Patients (
+
+--    PatientID INT PRIMARY KEY IDENTITY,
+
+--    Name NVARCHAR(50),
+
+--    Age INT,
+
+--    Disease NVARCHAR(100)
+
+--);
+
+-- SELECT *
+
+--CREATE TABLE Doctors (
+
+--    DoctorID INT PRIMARY KEY IDENTITY,
+
+--    Name NVARCHAR(50),
+
+--    Specialization NVARCHAR(50)
+
+--);
+
+ 
+
+--CREATE TABLE Appointments (
+
+--    AppointmentID INT PRIMARY KEY IDENTITY,
+
+--    PatientID INT,
+
+--    DoctorID INT,
+
+--    AppointmentDate DATETIME
+
+--);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
